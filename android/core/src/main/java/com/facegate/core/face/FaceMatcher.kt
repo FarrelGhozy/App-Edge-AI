988c2928c1e8b1025f5b929ad9dc8364b20b232f
FarrelGhozy <farrelafif05@gmail.com>
2026-07-14 14:55:00 +0700

    menuruhkan sesifitas parameter

--- android/core/src/main/java/com/facegate/core/face/FaceMatcher.kt
+++ android/core/src/main/java/com/facegate/core/face/FaceMatcher.kt
@@ -7,7 +7,7 @@ data class MatchResult(
 )
 
 class FaceMatcher(
-    private val threshold: Float = 0.75f
+    private val threshold: Float = 0.7f
 ) : FaceIndex {
     private val faceIndex = mutableMapOf<String, FloatArray>()
 
