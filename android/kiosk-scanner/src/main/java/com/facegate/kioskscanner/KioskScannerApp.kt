ec107efa6d9a7ae126bb79027eac5ef382f596dc
FarrelGhozy <farrelafif05@gmail.com>
2026-07-15 20:18:42 +0700

    inject and initialize KioskInitializer on app startup

--- android/kiosk-scanner/src/main/java/com/facegate/kioskscanner/KioskScannerApp.kt
+++ android/kiosk-scanner/src/main/java/com/facegate/kioskscanner/KioskScannerApp.kt
@@ -1,7 +1,16 @@
 package com.facegate.kioskscanner
 
 import android.app.Application
+import com.facegate.kioskscanner.service.KioskInitializer
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class KioskScannerApp : Application()
+class KioskScannerApp : Application() {
+    @Inject lateinit var kioskInitializer: KioskInitializer
+
+    override fun onCreate() {
+        super.onCreate()
+        kioskInitializer.initialize(this)
+    }
+}
